8f553467458562b858d60d52792fc139fba5ccc2
sam0402 <sam0402@gmail.com>
2021-08-12 07:46:15 +0800

    Update install_lms_cdrom_plugin.sh

--- install_lms_cdrom_plugin.sh
+++ install_lms_cdrom_plugin.sh
@@ -12,6 +12,6 @@ wget https://raw.githubusercontent.com/sam0402/pcp-44.1KHz/master/cdrom-5.13.1-p
 wget https://raw.githubusercontent.com/sam0402/pcp-44.1KHz/master/cdrom-5.13.1-pcpEVL_44k.tcz.md5.txt
 wget https://raw.githubusercontent.com/sam0402/pcp-44.1KHz/master/cdrom-5.13.1-pcpEVL_48k.tcz
 wget https://raw.githubusercontent.com/sam0402/pcp-44.1KHz/master/cdrom-5.13.1-pcpEVL_48k.tcz.md5.txt
-tce-load -i pcp-cdda2wav.tcz
+tce-load -w -i pcp-cdda2wav.tcz
 cd /mnt/$TARGET/slimserver/prefs/plugin/
 wget -O - https://raw.githubusercontent.com/bpa-code/bpaplugins/master/CDplayer-linux-v111.ZIP | unzip -
